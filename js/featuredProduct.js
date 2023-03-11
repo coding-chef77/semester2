49129b64f81822220bd50cb15e63557a06b8bdf5
Heine Günther <heine_gunther@hotmail.com>
2023-03-11 23:03:59 +0100

    updated bootstrap

--- js/featuredProduct.js
+++ js/featuredProduct.js
@@ -4,9 +4,13 @@ import { displayMessage } from "./ui/displayMessage.js";
 const container = document.querySelector(".featured-container");
 const heroContainer = document.querySelector(".hero-image");
 
+<<<<<<< HEAD
 const productUrl = baseUrl + "?populate=image";
 const imageUrl = "http://localhost:1337";
 const heroUrl = "http://localhost:1337/api/landing-page-image?populate=*";
+=======
+const productUrl = baseUrl + "populate=image";
+>>>>>>> parent of 26e478b (Changed patways)
 
 async function getProducts() {
   try {
@@ -33,14 +37,22 @@ async function getProducts() {
     container.innerHTML = "";
 
     products.forEach((speaker) => {
+<<<<<<< HEAD
       if (speaker.attributes.producyOfWeek) {
+=======
+      if (speaker.attributes.featured) {
+>>>>>>> parent of 26e478b (Changed patways)
         container.innerHTML += `
                               <div class="col-12 col-md-6 col-lg-4">   
                                 <div class="card">                             
                                 <img alt="picture related to product" class="card-img-top" src=${imageUrl}${speaker.attributes.image.data[0].attributes.url} width="200" />
                                 <div class="card-body">
+<<<<<<< HEAD
                                   <h5 class="card-title">${speaker.attributes.title}</h5>   
                                   <p>${speaker.attributes.description}</p>                             
+=======
+                                  <h5 class="card-title">${speaker.attributes.title}</h5>                                
+>>>>>>> parent of 26e478b (Changed patways)
                                   <p>$ ${speaker.attributes.price}</p>
                                   
                                   <a href="product-details.html?id=${speaker.attributes.id}" class="btn btn-primary">See more</a>
--- js/ui/renderProducts.js
+++ js/ui/renderProducts.js
@@ -7,10 +7,17 @@ export function renderProducts(products) {
     resultContainer.innerHTML += `
                                   <div class="col-12 col-md-6 col-lg-4">
                                     <div class="card">
+<<<<<<< HEAD
                                     <img alt="picture related to product" class="card-img-top" src=${imageUrl}${product.attributes.image.data[0].attributes.url} width="200" />
                                     <div class="card-body">
                                       <h5 class="card-title">${product.attributes.title}</h5>
                                       <p>${product.attributes.description}</p>
+=======
+                                    <img alt="picture related to product" class="card-img-top" src=${product.attributes.image}/>
+                                    <div class="card-body">
+                                      <h5 class="card-title">${product.attributes.title}</h5>
+                                      
+>>>>>>> parent of 26e478b (Changed patways)
                                       <p>$ ${product.attributes.price}</p>
                                       <a href="product-details.html?id=${product.attributes.id}" class="btn btn-primary">See more</a>
                                       </div>
