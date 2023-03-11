fe01bd94cd44b32d711274a2af150838fe25e247
Heine Günther <heine_gunther@hotmail.com>
2023-03-11 22:20:19 +0100

    updated hero

--- js/featuredProduct.js
+++ js/featuredProduct.js
@@ -2,26 +2,45 @@ import { baseUrl } from "./setting/api.js";
 import { displayMessage } from "./ui/displayMessage.js";
 
 const container = document.querySelector(".featured-container");
+const heroContainer = document.querySelector(".hero-image");
 
 const productUrl = baseUrl + "?populate=image";
+const imageUrl = "http://localhost:1337";
+const heroUrl = "http://localhost:1337/api/landing-page-image?populate=*";
 
 async function getProducts() {
+  try {
+    const response = await fetch(heroUrl);
+    const result = await response.json();
+    const hero = result.data;
+    console.log(hero);
+
+    heroContainer.innerHTML = `
+      <div class="hero-image"
+        style="background-image: url(${imageUrl}${hero.attributes.frontImage.data.attributes.url})"
+      ></div>
+    `;
+  } catch (error) {
+    console.error(error);
+  }
+
   try {
     const response = await fetch(productUrl);
     const results = await response.json();
 
     const products = results.data;
-    console.log(products);
+
     container.innerHTML = "";
 
     products.forEach((speaker) => {
-      if (speaker.attributes.ProducyOfWeek) {
+      if (speaker.attributes.producyOfWeek) {
         container.innerHTML += `
                               <div class="col-12 col-md-6 col-lg-4">   
                                 <div class="card">                             
-                                <img alt="picture related to product" class="card-img-top" src=${speaker.attributes.image.data[0].attributes.url}/>
+                                <img alt="picture related to product" class="card-img-top" src=${imageUrl}${speaker.attributes.image.data[0].attributes.url} width="200" />
                                 <div class="card-body">
-                                  <h5 class="card-title">${speaker.attributes.name}</h5>                                
+                                  <h5 class="card-title">${speaker.attributes.title}</h5>   
+                                  <p>${speaker.attributes.description}</p>                             
                                   <p>$ ${speaker.attributes.price}</p>
                                   
                                   <a href="product-details.html?id=${speaker.attributes.id}" class="btn btn-primary">See more</a>
